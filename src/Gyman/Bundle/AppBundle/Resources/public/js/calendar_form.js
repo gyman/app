558eb97f5eb092363674a72d37b047c25b6f877c
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2015-11-19 02:08:16 +0100

    javascript for calendar

---
+++ src/Gyman/Bundle/AppBundle/Resources/public/js/calendar_form.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    var TYPE_SINGLE = "single";
+    var TYPE_WEEKLY = "weekly";
+
+    var $calendarNameElement = $("select#create_event_calendar, select#update_event_calendar");
+    var $newCalendarNameElement = $("input#create_event_new_calendar_name, input#update_event_new_calendar_name");
+
+    var $eventTypeElement = $("select#create_event_type, select#update_event_type");
+    var $repetitionCheckboxes = $("label[for^='create_event_repetitionDays_'], label[for^='update_event_repetitionDays_']").parents('div.control-group');
+    var $endDateElement = $("label[for='create_event_endDate'], label[for='update_event_endDate'], input#create_event_endDate, input#update_event_endDate").parents('div.control-group');
+
+    var typeChangeEventName = "change.calendar_event_type";
+    var calendarNameChangeEventName = "change.calendar_name";
+
+    // handlers
+
+    var eventCalendarNameChangeHandler = function(event) {
+        var $target = $(event.target);
+        var value = $target.val();
+
+        if(value === "") {
+            $newCalendarNameElement.parents("div.control-group").show();
+        } else {
+            $newCalendarNameElement.parents("div.control-group").hide();
+        }
+
+        event.preventDefault();
+    }
+
+    var eventTypeChangeHandler = function(event) {
+        var $target = $(event.target);
+        var value = $target.val();
+
+        if(value === TYPE_SINGLE) {
+            $repetitionCheckboxes.hide();
+            $endDateElement.hide();
+        } else if (value === TYPE_WEEKLY)
+        {
+            $repetitionCheckboxes.show();
+            $endDateElement.show();
+        }
+
+        event.preventDefault();
+    };
+
+    // events
+
+    $eventTypeElement.off(typeChangeEventName).on(typeChangeEventName, eventTypeChangeHandler);
+    $eventTypeElement.trigger(typeChangeEventName);
+
+    $calendarNameElement.off(calendarNameChangeEventName).on(calendarNameChangeEventName, eventCalendarNameChangeHandler);
+    $calendarNameElement.trigger(calendarNameChangeEventName);
+});
