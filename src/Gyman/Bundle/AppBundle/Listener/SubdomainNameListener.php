dd2d225b864627c8070eec2b2f33b64bfc057b71
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2017-03-06 00:39:20 +0000

    refactored club listener

--- src/Gyman/Bundle/AppBundle/Listener/SubdomainNameListener.php
+++ src/Gyman/Bundle/AppBundle/Listener/SubdomainNameListener.php
@@ -2,17 +2,8 @@
 namespace Gyman\Bundle\AppBundle\Listener;
 
 use Dende\MultitenancyBundle\Manager\TenantManager;
-use Gyman\Bundle\AppBundle\Globals;
 use Gyman\Bundle\AppBundle\Services\SubdomainProviderInterface;
-use Gyman\Bundle\AppBundle\Services\TenantProvider;
-use Gyman\Bundle\ClubBundle\Entity\ClubRepository;
-use Gyman\Bundle\ClubBundle\Entity\Subdomain;
-use Symfony\Component\DependencyInjection\Container;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\Routing\Router;
-use Twig_Environment;
 
 /**
  * Class SubdomainNameListener
@@ -31,34 +22,14 @@ class SubdomainNameListener
     private $tenantManager;
 
     /**
-     * @var TenantProvider
-     */
-    private $tenantProvider;
-
-    /**
-     * @var Twig_Environment
-     */
-    private $twig;
-
-    /**
-     * @var ClubRepository
-     */
-    private $clubRepository;
-
-    /**
      * SubdomainNameListener constructor.
      * @param SubdomainProviderInterface $subdomainProvider
      * @param TenantManager $tenantManager
-     * @param TenantProvider $tenantProvider
-     * @param Twig_Environment $twig
      */
-    public function __construct(SubdomainProviderInterface $subdomainProvider, TenantManager $tenantManager, TenantProvider $tenantProvider, ClubRepository $clubRepository, Twig_Environment $twig)
+    public function __construct(SubdomainProviderInterface $subdomainProvider, TenantManager $tenantManager)
     {
         $this->subdomainProvider = $subdomainProvider;
         $this->tenantManager = $tenantManager;
-        $this->tenantProvider = $tenantProvider;
-        $this->clubRepository = $clubRepository;
-        $this->twig = $twig;
     }
 
     public function onKernelRequest(GetResponseEvent $event)
@@ -68,33 +39,6 @@ class SubdomainNameListener
         }
 
         $subdomainName = $this->subdomainProvider->getSubdomain();
-
         $this->tenantManager->switchConnection('tenant', $subdomainName);
-
-
-        $club = $this->clubRepository->findOneBySubdomain($subdomainName);
-        /*
-
-        if(strlen($subdomainName) > 0 && is_null($club)) {
-            $schemaAndHost = strtr($event->getRequest()->getSchemeAndHttpHost(), [$subdomainName."." => ""]);
-
-            $path = $this->container->get("router")->generate(
-                "gyman_error_club_not_found",
-                ["subdomain" => $subdomainName],
-                Router::ABSOLUTE_PATH
-            );
-
-            $event->setResponse(new RedirectResponse($schemaAndHost . $path));
-            return;
-        }
-
-        Globals::setNoImage($this->container->getParameter('no_image'));
-        Globals::setGalleryDir($this->container->getParameter('gallerydirectory') . $subdomainName . DIRECTORY_SEPARATOR);
-        Globals::setGalleryPath($this->container->getParameter('gallerypath') . $subdomainName . DIRECTORY_SEPARATOR);
-        Globals::setSubdomain($subdomainName);
-
-        $this->container->get('session')->set('current_club', $club);
-        */
-        $this->twig->addGlobal('club', $club);
     }
 }
