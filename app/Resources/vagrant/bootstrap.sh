c851cbeededc233b10eed9fccef1fcaa1f3f2809
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2017-06-18 17:40:06 +0200

    removed composer checksum

--- app/Resources/vagrant/bootstrap.sh
+++ app/Resources/vagrant/bootstrap.sh
@@ -67,14 +67,12 @@ less /vagrant/app/Resources/vagrant/.bash_aliases >> /home/vagrant/.bash_aliases
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '55d6ead61b29c7bdee5cccfb50076874187bd9f21f65d8991d46ec5cc90518f447387fb9f76ebae1fbbacf329e583e30') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 mv composer.phar /usr/bin/composer
 
 su vagrant <<'EOF'
 cd /vagrant
-exit
 composer install --prefer-source --no-interaction
 
 php app/console doctrine:schema:create
